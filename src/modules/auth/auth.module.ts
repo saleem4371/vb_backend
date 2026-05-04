d84b2e790e3cc0941a314267ac8fc83f769df0d8
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-05-04 16:31:13 +0530

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { MailModule } from '../../mail/mail.module';
-import { JwtStrategy } from './jwt/jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { CommonModule } from '../../common/common.module';
 
 @Module({
