847ebd0dab0b9e3d03b4eae0968877d6dcd26546
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-05-05 10:30:11 +0530

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -62,8 +62,9 @@ export class AuthController {
 
   // ================= FORGOT PASSWORD =================
   @Post('forgot_password')
-  async forgot_password(@Body() dto: any) {
-    return this.authService.forgot_password(dto);
+  async forgot_password(@Body() dto: { email: string }) {
+    const otp =  Math.floor(100000 + Math.random() * 900000).toString();
+    return this.authService.forgot_password(otp,dto);
   }
 
   // ================= UPDATE PASSWORD =================
