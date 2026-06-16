0d569bd7314e63760534381cbf7df04e78a1a086
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-06-16 13:07:02 +0530

    Update push.controller.ts

--- src/push/push.controller.ts
+++ src/push/push.controller.ts
@@ -14,8 +14,8 @@ import {
 } from '@nestjs/common';
 import type { FastifyRequest } from 'fastify';
 
-import { JwtAuthGuard } from '../../modules/auth/strategies/jwt-auth.guard';
-import { CurrentUser } from '../../common/decorators/user.decorator';
+import { JwtAuthGuard } from '../modules/auth/strategies/jwt-auth.guard';
+import { CurrentUser } from '../common/decorators/user.decorator';
 import { PushService } from './push.service';
 import { RegisterPushDto } from './dto/register-push.dto';
 
