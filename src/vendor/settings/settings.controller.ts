da9fdd7d3aeed9d511686a8947d8dbc481b1a918
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-06-16 13:10:49 +0530

    Update settings.controller.ts

--- src/vendor/settings/settings.controller.ts
+++ src/vendor/settings/settings.controller.ts
@@ -14,8 +14,8 @@ import {
 } from '@nestjs/common';
 import type { FastifyRequest } from 'fastify';
 
-import { JwtAuthGuard } from '../../../modules/auth/strategies/jwt-auth.guard';
-import { CurrentUser } from '../../../common/decorators/user.decorator';
+import { JwtAuthGuard } from '../../modules/auth/strategies/jwt-auth.guard';
+import { CurrentUser } from '../../common/decorators/user.decorator';
 import { SettingsService } from './settings.service';
 
 @Controller('settings')
