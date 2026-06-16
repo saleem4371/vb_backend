3f7cb161696aef9e19eff8b2889f010095a5b2e4
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-06-16 13:21:14 +0530

    Delete src/vendor directory

--- src/vendor/kyc/kyc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KycController } from './kyc.controller';
-import { KycService } from './kyc.service';
-
-@Module({
-  controllers: [KycController],
-  providers: [KycService]
-})
-export class KycModule {}
