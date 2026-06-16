8b597b9cb2c27448c60a3d51c9cdb4e4f9b7f25e
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-06-16 13:14:29 +0530

    Update venue-listing.module.ts

--- src/vendor/venue-listing/venue-listing.module.ts
+++ src/vendor/venue-listing/venue-listing.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { VenueListingController } from './venue-listing.controller';
 import { VenueListingService } from './venue-listing.service';
-import { VenueChild } from '../../../modules/listing/entities/venue-child.entity';
+import { VenueChild } from '../../modules/listing/entities/venue-child.entity';
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
