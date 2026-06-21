3667623ac2b25da4ae3ca94bd63f1b0c6b69a23f
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-06-21 18:14:32 +0530

    Update cashfree.controller.ts

--- src/modules/integrations/cashfree/cashfree.controller.ts
+++ src/modules/integrations/cashfree/cashfree.controller.ts
@@ -10,6 +10,7 @@ import {
   Body,
   Query,
   Delete,
+  Headers,
 } from '@nestjs/common';
 import type { FastifyRequest } from 'fastify';
 
@@ -18,30 +19,25 @@ import { CashfreeService } from './cashfree.service';
 import { JwtAuthGuard } from '../../../modules/auth/strategies/jwt-auth.guard';
 import { CurrentUser } from '../../../common/decorators/user.decorator';
 
-
 @Controller('cashfree')
 export class CashfreeController {
   constructor(private readonly cashfreeService: CashfreeService) {}
 
   @UseGuards(JwtAuthGuard)
- @Post('subscription')
-subscription(@Body() body: any,@CurrentUser() user: any) {
-  return this.cashfreeService.subscription(body,user?.id);
-}
+  @Post('subscription')
+  subscription(@Body() body: any, @CurrentUser() user: any,@Headers('x-country') Country : any) {
+    return this.cashfreeService.subscription(body, user?.id,Country);
+  }
 
-@Get("verify_subscription/:subscription_id")
-async verifySubscription(
-  @Param("subscription_id") subscriptionId: string
-) {
-  
-  return this.cashfreeService.verifySubscription(subscriptionId);
-}
-@Get("cashfree_plans/:category")
-async cashfree_plans(
-  @Param("category") category_id: string, @Query('category') category: string,
-) {
-  
-  return this.cashfreeService.cashfree_plans(category_id,category);
-}
-
+  @Get('verify_subscription/:subscription_id')
+  async verifySubscription(@Param('subscription_id') subscriptionId: string) {
+    return this.cashfreeService.verifySubscription(subscriptionId);
+  }
+  @Get('cashfree_plans/:category')
+  async cashfree_plans(
+    @Param('category') category_id: string,
+    @Query('category') category: string,
+  ) {
+    return this.cashfreeService.cashfree_plans(category_id, category);
+  }
 }
