e02eec009b96689bf78ea572ec0c772d209650b6
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-07-21 14:48:50 +0530

    Update home.controller.ts

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get , UseGuards ,Headers} from '@nestjs/common';
+import { Controller, Get, UseGuards, Headers, Query } from '@nestjs/common';
 import { HomeService } from './home.service';
 
 import { JwtAuthGuard } from '../../modules/auth/strategies/jwt-auth.guard';
@@ -11,25 +11,43 @@ export class HomeController {
   @Get()
   getHome() {
     return this.homeService.getHomePageData();
-  } 
-   @UseGuards(JwtAuthGuard)
+  }
+  @UseGuards(JwtAuthGuard)
   @Get('recent_views')
-  getUserRecentViews(@CurrentUser() user: any) {
+  getUserRecentViews(
+    @CurrentUser() user: any,
+    @Headers('x-category') category: any,
+  ) {
     const userId = user?.id;
-    return this.homeService.getUserRecentViews(userId);
+    return this.homeService.getUserRecentViews(userId, category);
   }
 
-   @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('vendor_category')
   vendor_category(
-    @CurrentUser() user: any,@Headers('x-country') country : any) {
+    @CurrentUser() user: any,
+    @Headers('x-country') country: any,
+  ) {
     const userId = user?.id;
-    return this.homeService.vendor_category(userId,country);
-  }  
-  
+    return this.homeService.vendor_category(userId, country);
+  }
+
   @Get('recommeded')
-  recommeded_property( @Headers('x-country') country : any) {
+  recommeded_property(
+    @Headers('x-country') country: any,
+    @Query('lat') lat: number,
+    @Query('lng') lng: number,
+    @Query('state') state: string,
+    @Headers('x-category') category: any,
+  ) {
     // const userId = user?.id;
-    return this.homeService.recommeded_property();
+
+    console.log(state);
+    return this.homeService.recommeded_property(state, category);
+  }
+
+  @Get('topDestination')
+  async getNearbyCities(@Query('state') state: string) {
+    return this.homeService.getNearbyCities(state);
   }
 }
