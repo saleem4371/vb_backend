27719fa28f8a01974444c2f944ffb10957a634c8
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-07-29 11:20:30 +0530

    Update venue.module.ts

--- src/modules/venue/venue.module.ts
+++ src/modules/venue/venue.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
-import { VenueService } from './venue.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VenueController } from './venue.controller';
+import { VenueService } from './venue.service';
+
 import { VenueChild } from '../../modules/listing/entities/venue-child.entity';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { SocketModule } from '../socket/socket.module';
 
 @Module({
-  providers: [VenueService],
-  controllers: [VenueController],
-  exports: [VenueService],
-    imports: [
+  imports: [
+    SocketModule,
     TypeOrmModule.forFeature([
       VenueChild,
     ]),
   ],
+  controllers: [VenueController],
+  providers: [VenueService],
+  exports: [VenueService],
 })
 export class VenueModule {}
