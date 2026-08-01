99935beca2106a0fff2b5cc1ea4971f87e3168d5
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-08-01 08:26:10 +0530

    Update venue-listing.module.ts

--- src/modules/vendor/venue-listing/venue-listing.module.ts
+++ src/modules/vendor/venue-listing/venue-listing.module.ts
@@ -3,13 +3,13 @@ import { VenueListingController } from './venue-listing.controller';
 import { VenueListingService } from './venue-listing.service';
 import { VenueChild } from '../../../modules/listing/entities/venue-child.entity';
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { SocketModule } from '../../socket/socket.module';
+//import { SocketModule } from '../../socket/socket.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       VenueChild,
-      SocketModule
+     // SocketModule
     ]),
   ],
   controllers: [VenueListingController],
