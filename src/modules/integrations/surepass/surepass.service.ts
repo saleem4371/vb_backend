b4d667e363c3b7e4afbbd7ccc8044c93c944025e
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-08-21 10:13:32 +0530

    Refactor category data retrieval and assignment
    Comment out category data query and update category_id assignment.

--- src/modules/integrations/surepass/surepass.service.ts
+++ src/modules/integrations/surepass/surepass.service.ts
@@ -297,17 +297,17 @@ export class SurepassService {
       ? JSON.parse(config)
       : config;
 
-     const singular = ctx.categoryId.endsWith('s') ? ctx.categoryId.slice(0, -1) : ctx.categoryId;
+    //  const singular = ctx.categoryId.endsWith('s') ? ctx.categoryId.slice(0, -1) : ctx.categoryId;
 
-    const [categoryData] = await this.dataSource.query(
-      `SELECT * FROM category WHERE name = ? LIMIT 1`,
-      [singular],
-    );
+    // const [categoryData] = await this.dataSource.query(
+    //   `SELECT * FROM category WHERE name = ? LIMIT 1`,
+    //   [singular],
+    // );
 
   const state = JSON.stringify({
     user_id: ctx.userId,
     country_id: ctx.countryId,
-    category_id: categoryData.id,
+    category_id:  ctx.countryId,
   });
 
   const redirectUrl =
